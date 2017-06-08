88044d545128375e59fae447674fd22958d78e4d
davydka <davydka@gmail.com>
2017-06-08 17:14:18 -0400

    adjusting row height

--- src/ofxTeensyOcto.cpp
+++ src/ofxTeensyOcto.cpp
@@ -1,83 +1,88 @@
 #include "ofxTeensyOcto.h"
 
 //--------------------------------------------------------------
-void ofxTeensyOcto::setup(int _ledWidth, int _ledHeight, int _stripsPerPort, int _numPorts)
+void ofxTeensyOcto::setup(int _ledWidth, int _ledHeight, int _rowHeight, int _stripsPerPort, int _numPorts)
 {
-    // LED variables
-    ledWidth = _ledWidth;                       // LED max width
-    ledHeight = _ledHeight;                     // LED max height
-    stripsPerPort = _stripsPerPort;             // LED strips per teensy
-    numPortsMain = _numPorts;                   // number of teensy ports
-    numPorts = 0;                               // teensy ports counter
-    maxPorts = 8;                               // max teensy ports
-    simulate = false;                           // simulate the teensy buffers
+	// LED variables
+	ledWidth = _ledWidth;                       // LED max width
+	ledHeight = _ledHeight;                     // LED max height
+	stripsPerPort = _stripsPerPort;             // LED strips per teensy
+	numPortsMain = _numPorts;                   // number of teensy ports
+	numPorts = 0;                               // teensy ports counter
+	maxPorts = 8;                               // max teensy ports
+	simulate = false;                           // simulate the teensy buffers
 
-    brightness = 0.5;
+	// octows2811 library takes row heights as divisions of 8, for some reason yet unknown
+	// i.e. stripHeight = 96, this translates to 12 rows (96/8 = 12)
+	// set this value for teensy.draw() to reflect 1 row = 1px tall, for better previewing
+	rowHeight = _rowHeight;
 
-    dataSize = ((ledWidth * (ledHeight * stripsPerPort)) * 3) + 3;
-    
-    // LED arrays
-    ledSerial = new ofSerial[maxPorts];
-    ledArea = new ofRectangle[maxPorts];
-    ledLayout = new bool[maxPorts];
-    ledImage = new ofImage[maxPorts];
-    colors = new ofColor[ledWidth * (ledHeight * stripsPerPort * numPortsMain)];
-    pixels1.allocate(ledWidth, ledHeight*stripsPerPort*numPortsMain, OF_PIXELS_RGB);
-    pixels2.allocate(ledWidth, ledHeight*stripsPerPort*numPortsMain, OF_PIXELS_RGB);
-    ledData = new unsigned char[((ledWidth * (ledHeight * stripsPerPort)) * 3) + 3];
-    
-    // let's list our serial devices
-    ledSerial[numPorts].listDevices();
-    vector <ofSerialDeviceInfo> deviceList = ledSerial[numPorts].getDeviceList();
-    ofSleepMillis(20);    
+	brightness = 0.5;
+
+	dataSize = ((ledWidth * (ledHeight * stripsPerPort)) * 3) + 3;
+
+	// LED arrays
+	ledSerial = new ofSerial[maxPorts];
+	ledArea = new ofRectangle[maxPorts];
+	ledLayout = new bool[maxPorts];
+	ledImage = new ofImage[maxPorts];
+	colors = new ofColor[ledWidth * (ledHeight * stripsPerPort * numPortsMain)];
+	pixels1.allocate(ledWidth, ledHeight*stripsPerPort*numPortsMain, OF_PIXELS_RGB);
+	pixels2.allocate(ledWidth, ledHeight*stripsPerPort*numPortsMain, OF_PIXELS_RGB);
+	ledData = new unsigned char[((ledWidth * (ledHeight * stripsPerPort)) * 3) + 3];
+
+	// let's list our serial devices
+	ledSerial[numPorts].listDevices();
+	vector <ofSerialDeviceInfo> deviceList = ledSerial[numPorts].getDeviceList();
+	ofSleepMillis(20);
 }
 
 //--------------------------------------------------------------
 void ofxTeensyOcto::serialConfigure(string portName, float _xoffset, float _yoffset, float _widthPct, float _heightPct, int _direction)
 {
-    int baud = 115200;
-    ledSerial[numPorts].setup(portName, baud);
-    ledSerial[numPorts].writeByte('?');         // send an initial character
-    ofSleepMillis(50);
-    
-    string configline;
-    while (ledSerial[numPorts].available() != OF_SERIAL_ERROR ){
-        int val = ledSerial[numPorts].readByte();
-        if(val == 10) {
-            break;
-        }
-        configline += val;
-    }
+	int baud = 115200;
+	ledSerial[numPorts].setup(portName, baud);
+	ledSerial[numPorts].writeByte('?');         // send an initial character
+	ofSleepMillis(50);
 
-    if (configline.length() <= 0) {
-      ofLogError("serialConfigure") << "Serial port " + portName + " is not responding.";
-      ofLogError("serialConfigure") << "Is it really a Teensy 3.0+ running VideoDisplay?";
-      simulate = true;
-    } else {
-        ofLogNotice() << "Read " << configline.length() << " bytes: " << configline  << endl;
-    }
+	string configline;
+	while (ledSerial[numPorts].available() != OF_SERIAL_ERROR ){
+		int val = ledSerial[numPorts].readByte();
+		if(val == 10) {
+			break;
+		}
+		configline += val;
+	}
 
-    // Count the number of parameters received
-    int numparam = 0;
-    char *dup = strdup(configline.c_str());
-    char *token = strtok(dup, ",");
-    while (token != NULL) {
-        token = strtok(NULL, ",");
-        numparam++;
-    }
-    free(dup);
-    ofLogNotice("serialConfigure") << "Number of parameters: " << numparam;
+	if (configline.length() <= 0) {
+	  ofLogError("serialConfigure") << "Serial port " + portName + " is not responding.";
+	  ofLogError("serialConfigure") << "Is it really a Teensy 3.0+ running VideoDisplay?";
+	  simulate = true;
+	} else {
+		ofLogNotice() << "Read " << configline.length() << " bytes: " << configline  << endl;
+	}
 
-    if (numparam != 12) {
-      ofLogError("serialConfigure") << "Error: port " + portName + " did not respond to LED config query";
-      simulate = true;
-    }
+	// Count the number of parameters received
+	int numparam = 0;
+	char *dup = strdup(configline.c_str());
+	char *token = strtok(dup, ",");
+	while (token != NULL) {
+		token = strtok(NULL, ",");
+		numparam++;
+	}
+	free(dup);
+	ofLogNotice("serialConfigure") << "Number of parameters: " << numparam;
 
-    // only store the info and increase numPorts if Teensy responds properly
-    ledImage[numPorts].allocate(ledWidth, ledHeight * stripsPerPort, OF_IMAGE_COLOR);
-    ledArea[numPorts].set(_xoffset, _yoffset, _widthPct, _heightPct);
-    ledLayout[numPorts] = _direction == 0; // affects layout > pixel direction
-    numPorts++;
+	if (numparam != 12) {
+	  ofLogError("serialConfigure") << "Error: port " + portName + " did not respond to LED config query";
+	  simulate = true;
+	}
+
+	// only store the info and increase numPorts if Teensy responds properly
+	ledImage[numPorts].allocate(ledWidth, ledHeight * stripsPerPort, OF_IMAGE_COLOR);
+	ledArea[numPorts].set(_xoffset, _yoffset, _widthPct, _heightPct);
+	ledLayout[numPorts] = _direction == 0; // affects layout > pixel direction
+	numPorts++;
 }
 
 
@@ -87,119 +92,123 @@ void ofxTeensyOcto::serialConfigure(string portName, float _xoffset, float _yoff
 //--------------------------------------------------------------
 void ofxTeensyOcto::image2data(ofImage image, unsigned char* data, bool layout)
 {    
-    int offset = 3;
-    int x, y, xbegin, xend, xinc, mask;
-    int linesPerPin = image.getHeight() / 8;
-    int* pixel = new int[8];
-    
-    // get the copied image pixels
-    pixels2 = image.getPixels();
-    pixels2.setNumChannels(3);
+	int offset = 3;
+	int x, y, xbegin, xend, xinc, mask;
+	int linesPerPin = image.getHeight() / 8;
+	int* pixel = new int[8];
+
+	// get the copied image pixels
+	pixels2 = image.getPixels();
+	pixels2.setNumChannels(3);
 
 
-    // 2d array of our pixel colors
-    for (int x = 0; x < ledWidth; x++)
-    {
-        for (int y = 0; y < (ledHeight * stripsPerPort * numPortsMain); y++)
-        {
-            int loc = x + y * ledWidth;
-            ofColor c = pixels2.getColor(x, y);
-            c *= brightness;
-            colors[loc] = c;
-            //colors[loc] = pixels2.getColor(x, y);
-        }
-    }
-    
-    for (y = 0; y < linesPerPin; y++)
-    {
-        if ((y & 1) == (layout ? 0 : 1))
-        {
-            // even numbered rows are left to right
-            xbegin = 0;
-            xend = image.getWidth();
-            xinc = 1;
-        }
-        else
-        {
-            // odd numbered rows are right to left
-            xbegin = image.getWidth() - 1;
-            xend = -1;
-            xinc = -1;
-        }
-        
-        for (x = xbegin; x != xend; x += xinc)
-        {
-            for (int i=0; i < 8; i++)
-            {
-                int temploc = x + (y + linesPerPin * i) * image.getWidth();
-                pixel[i] = colors[temploc].getHex();
-                pixel[i] = colorWiring(pixel[i]);
-            }
-            
-            // convert 8 pixels to 24 bytes
-            for (mask = 0x800000; mask != 0; mask >>= 1)
-            {
-                unsigned char b = 0;
-                for (int i=0; i < 8; i++)
-                {
-                    if ((pixel[i] & mask) != 0) b |= (1 << i);
-                }
-                data[offset++] = b;
-            }
-        }
-    }
+	// 2d array of our pixel colors
+	for (int x = 0; x < ledWidth; x++)
+	{
+		for (int y = 0; y < (ledHeight * stripsPerPort * numPortsMain); y++)
+		{
+			int loc = x + y * ledWidth;
+			ofColor c = pixels2.getColor(x, y);
+			c *= brightness;
+			colors[loc] = c;
+			//colors[loc] = pixels2.getColor(x, y);
+		}
+	}
+
+	for (y = 0; y < linesPerPin; y++)
+	{
+		if ((y & 1) == (layout ? 0 : 1))
+		{
+			// even numbered rows are left to right
+			xbegin = 0;
+			xend = image.getWidth();
+			xinc = 1;
+		}
+		else
+		{
+			// odd numbered rows are right to left
+			xbegin = image.getWidth() - 1;
+			xend = -1;
+			xinc = -1;
+		}
+
+		for (x = xbegin; x != xend; x += xinc)
+		{
+			for (int i=0; i < 8; i++)
+			{
+				int temploc = x + (y + linesPerPin * i) * image.getWidth();
+				pixel[i] = colors[temploc].getHex();
+				pixel[i] = colorWiring(pixel[i]);
+			}
+
+			// convert 8 pixels to 24 bytes
+			for (mask = 0x800000; mask != 0; mask >>= 1)
+			{
+				unsigned char b = 0;
+				for (int i=0; i < 8; i++)
+				{
+					if ((pixel[i] & mask) != 0) b |= (1 << i);
+				}
+				data[offset++] = b;
+			}
+		}
+	}
 }
 
 //--------------------------------------------------------------
 void ofxTeensyOcto::draw(int x, int y)
 {
-    // pixel display (drawing not optimised!)
-    ofColor colors;
-    ofPushMatrix();
-    ofTranslate(x,y);
-    ofSetRectMode(OF_RECTMODE_CENTER);
-    for (int y = 0; y < ledHeight*stripsPerPort*numPorts; y++)
-    {
-        for (int x = 0; x < ledWidth; x++)
-        {
-            ofPushMatrix();
-            colors = pixels1.getColor(x, y);
-            ofSetColor(colors);
-            ofTranslate(x*2, y*2 + (y/16*4)); //sections in groups
-            ofDrawRectangle(x, y, 2, 2);
-            ofPopMatrix();
-        }
-    }
-    ofSetRectMode(OF_RECTMODE_CORNER);
-    ofPopMatrix();
+	// pixel display (drawing not optimised!)
+	ofColor colors;
+	ofPushMatrix();
+	ofTranslate(x,y);
+	ofSetRectMode(OF_RECTMODE_CENTER);
+	for (int y = 0; y < rowHeight*stripsPerPort*numPorts; y++)
+	{
+		for (int x = 0; x < ledWidth; x++)
+		{
+			ofPushMatrix();
+			colors = pixels1.getColor(x, y);
+			ofSetColor(colors);
+			if(rowHeight == ledHeight)
+				ofTranslate(x*2, y*2 + (y/16*4)); //sections in groups
+			else
+				ofTranslate(x*2, y*2);
+
+			ofDrawRectangle(x, y, 2, 2);
+			ofPopMatrix();
+		}
+	}
+	ofSetRectMode(OF_RECTMODE_CORNER);
+	ofPopMatrix();
 }
 
 //--------------------------------------------------------------
 void ofxTeensyOcto::update()
 {
-    if(simulate) return;
+	if(simulate) return;
 
-    // send our data via serial
-    for (int i=0; i < numPorts; i++)
-    {
-        // copy a portion of the movie's image to the LED image
-        float xoffset = percentage(ledWidth, ledArea[i].x);
-        float yoffset = percentage((ledHeight * stripsPerPort * numPortsMain), ledArea[i].y);
-        float xwidth =  percentage(ledWidth, ledArea[i].getWidth());
-        float yheight = percentage((ledHeight * stripsPerPort * numPortsMain), ledArea[i].getHeight());
+	// send our data via serial
+	for (int i=0; i < numPorts; i++)
+	{
+		// copy a portion of the movie's image to the LED image
+		float xoffset = percentage(ledWidth, ledArea[i].x);
+		float yoffset = percentage((ledHeight * stripsPerPort * numPortsMain), ledArea[i].y);
+		float xwidth =  percentage(ledWidth, ledArea[i].getWidth());
+		float yheight = percentage((ledHeight * stripsPerPort * numPortsMain), ledArea[i].getHeight());
 
-        // crop the pixels
-        ledImage[i].setFromPixels(pixels1);
-        ledImage[i].crop(xoffset, yoffset, xwidth, yheight);
+		// crop the pixels
+		ledImage[i].setFromPixels(pixels1);
+		ledImage[i].crop(xoffset, yoffset, xwidth, yheight);
 
-        image2data(ledImage[i], ledData, ledLayout[i]);
+		image2data(ledImage[i], ledData, ledLayout[i]);
 
-        ledData[0] = '*';  // first Teensy is the frame sync master
+		ledData[0] = '*';  // first Teensy is the frame sync master
 
-        int nBytesWritten = 0;
-        while (nBytesWritten < dataSize){
-            nBytesWritten += ledSerial[i].writeBytes( &ledData[nBytesWritten], dataSize );
-        }
-    }
+		int nBytesWritten = 0;
+		while (nBytesWritten < dataSize){
+			nBytesWritten += ledSerial[i].writeBytes( &ledData[nBytesWritten], dataSize );
+		}
+	}
 }
 
--- src/ofxTeensyOcto.h
+++ src/ofxTeensyOcto.h
@@ -5,73 +5,74 @@
 class ofxTeensyOcto
 {
 public:
-    
-    // teensy functions
-    void setup(int _ledWidth, int _ledHeight, int _stripsPerPort, int _numPorts);
-    void update();
-    void serialConfigure(string portName, float _xoffset, float _yoffset, float _widthPct, float _heightPct, int _direction);
-    void image2data(ofImage image, unsigned char* data, bool layout);
-    void draw(int x, int y);
 
-    // arrays
-    ofSerial * ledSerial;
-    ofRectangle * ledArea;
-    bool * ledLayout;           // layout of rows, true = even is left->right
-    ofImage * ledImage;         // image sent to each port
-    ofColor * colors;
-    unsigned char *ledData;
-    
-    // variables
-    ofPixels pixels1;
-    ofPixels pixels2;
-    int ledWidth;
-    int ledHeight;
-    int stripsPerPort;
-    int numPortsMain;
-    int numPorts;
-    int maxPorts;
-    int dataSize;
-    bool simulate;
+	// teensy functions
+	void setup(int _ledWidth, int _ledHeight, int _rowHeight, int _stripsPerPort, int _numPorts);
+	void update();
+	void serialConfigure(string portName, float _xoffset, float _yoffset, float _widthPct, float _heightPct, int _direction);
+	void image2data(ofImage image, unsigned char* data, bool layout);
+	void draw(int x, int y);
+
+	// arrays
+	ofSerial * ledSerial;
+	ofRectangle * ledArea;
+	bool * ledLayout;           // layout of rows, true = even is left->right
+	ofImage * ledImage;         // image sent to each port
+	ofColor * colors;
+	unsigned char *ledData;
+
+	// variables
+	ofPixels pixels1;
+	ofPixels pixels2;
+	int ledWidth;
+	int ledHeight;
+	int rowHeight;
+	int stripsPerPort;
+	int numPortsMain;
+	int numPorts;
+	int maxPorts;
+	int dataSize;
+	bool simulate;
 
 	void setBrightness(int b){ brightness = (float) ofClamp(b/255.0, 0.0, 1.0); }
-    float brightness;
+	float brightness;
 
-    // translate the 24 bit color from RGB to the actual
-    // order used by the LED wiring.  GRB is the most common.
-    inline int colorWiring(int c)
-    {
-        // GRB wiring
-        return ((c & 0xFF0000) >> 8) | ((c & 0x00FF00) << 8) | (c & 0x0000FF);
-    }
-    
-    // convert an integer from 0 to 100 to a float percentage
-    // from 0.0 to 1.0.  Special cases for 1/3, 1/6, 1/7, etc
-    // are handled automatically to fix integer rounding.
-    inline double percentageFloat(float percent)
-    {
-        if (percent == 33) return 1.0 / 3.0;
-        if (percent == 17) return 1.0 / 6.0;
-        if (percent == 14) return 1.0 / 7.0;
-        if (percent == 13) return 1.0 / 8.0;
-        if (percent == 11) return 1.0 / 9.0;
-        if (percent ==  9) return 1.0 / 11.0;
-        if (percent ==  8) return 1.0 / 12.0;
-        return (double)percent / 100.0;
-    }
-    
-    // scale a number by a percentage, from 0 to 100
-    inline float percentage(float num, float percent)
-    {
-        double mult = percentageFloat(percent);
-        double output = num * mult;
-        return (float)output;
-    }
-    
-    // scale a number by the inverse of a percentage, from 0 to 100
-    inline int percentageInverse(int num, int percent)
-    {
-        double div = percentageFloat(percent);
-        double output = num / div;
-        return (int)output;
-    }
+	// translate the 24 bit color from RGB to the actual
+	// order used by the LED wiring.  GRB is the most common.
+	inline int colorWiring(int c)
+	{
+		// GRB wiring
+		return ((c & 0xFF0000) >> 8) | ((c & 0x00FF00) << 8) | (c & 0x0000FF);
+	}
+
+	// convert an integer from 0 to 100 to a float percentage
+	// from 0.0 to 1.0.  Special cases for 1/3, 1/6, 1/7, etc
+	// are handled automatically to fix integer rounding.
+	inline double percentageFloat(float percent)
+	{
+		if (percent == 33) return 1.0 / 3.0;
+		if (percent == 17) return 1.0 / 6.0;
+		if (percent == 14) return 1.0 / 7.0;
+		if (percent == 13) return 1.0 / 8.0;
+		if (percent == 11) return 1.0 / 9.0;
+		if (percent ==  9) return 1.0 / 11.0;
+		if (percent ==  8) return 1.0 / 12.0;
+		return (double)percent / 100.0;
+	}
+
+	// scale a number by a percentage, from 0 to 100
+	inline float percentage(float num, float percent)
+	{
+		double mult = percentageFloat(percent);
+		double output = num * mult;
+		return (float)output;
+	}
+
+	// scale a number by the inverse of a percentage, from 0 to 100
+	inline int percentageInverse(int num, int percent)
+	{
+		double div = percentageFloat(percent);
+		double output = num / div;
+		return (int)output;
+	}
 };
